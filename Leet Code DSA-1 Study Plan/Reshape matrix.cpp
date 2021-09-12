1a6ed0393a731d21cddb6e6cb087a2cd286e7335
Shivam Shukla <77653717+shivam910@users.noreply.github.com>
2021-09-12 19:29:17 +0530

    Create Reshape matrix.cpp

---
+++ Leet Code DSA-1 Study Plan/Reshape matrix.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<vector<int>> matrixReshape(vector<vector<int>>& nums, int r, int c) {
+        int n= nums.size(), m= nums[0].size();
+        if(r*c != n*m)
+            return nums;
+        vector<vector<int>> res (r,vector<int> (c));
+            for(int i=0; i<n*m; i++)
+            {
+                res[i/c][i%c] = nums[i/m][i%m];
+            }
+        return res;
+    }
+};
