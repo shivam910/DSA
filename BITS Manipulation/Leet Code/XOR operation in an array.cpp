2908ff68a0c1a6ec7971ac20750160ba878f374d
Shivam Shukla <77653717+shivam910@users.noreply.github.com>
2021-09-19 00:49:36 +0530

    Create XOR operation in an array.cpp

---
+++ BITS Manipulation/Leet Code/XOR operation in an array.cpp
@@ -0,0 +1,18 @@
+// Given an integer n and an integer start.
+
+// Define an array nums where nums[i] = start + 2*i (0-indexed) and n == nums.length.
+
+// Return the bitwise XOR of all elements of nums.
+
+class Solution {
+public:
+    int xorOperation(int n, int start) {
+        int k;
+         k=(start + 2*0);
+        for(int i=1; i< n; i++)
+        {
+           k^=(start+ 2*i);
+        }
+        return k;
+    }
+};
