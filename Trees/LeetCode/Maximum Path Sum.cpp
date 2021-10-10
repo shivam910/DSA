83d05f257b813f3c22aa06d11e2449f976233fff
Shivam Shukla <77653717+shivam910@users.noreply.github.com>
2021-10-11 00:40:33 +0530

    Update Maximum Path Sum.cpp

--- Trees/LeetCode/Maximum Path Sum.cpp
+++ Trees/LeetCode/Maximum Path Sum.cpp
@@ -1,28 +1,16 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     int maxPathSum(TreeNode* root) {
-        int maxi = INT_MIN;
-        check(root, maxi);
-        return maxi;
+       int maxi = INT_MIN; 
+        check(root, maxi); 
+        return maxi;  
     }
-    int check( TreeNode* node, int &maxi) {
-        if(!node) return 0;
-        
-        int lh = (0, check(node -> left, maxi));
-        int rh = (0, check(node -> right, maxi));
-        
-        maxi = max( maxi, lh+rh+ node -> val);
-        return max(lh,rh) +node->val;
+    
+    int check(TreeNode* node, int &maxi) {
+        if (node == NULL) return 0;
+        int left = max(0, check(node->left, maxi));
+        int right = max(0, check(node->right, maxi));
+        maxi = max(maxi, left + right + node->val);
+        return max(left, right) + node->val;
     }
 };
