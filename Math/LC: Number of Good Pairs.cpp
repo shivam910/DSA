7c7f9934274c7bcbb3ff364a9dff1b41dbbd05fc
Shivam Shukla <77653717+shivam910@users.noreply.github.com>
2021-10-17 16:01:45 +0530

    Create LC: Number of Good Pairs.cpp

---
+++ Math/LC: Number of Good Pairs.cpp
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/number-of-good-pairs/submissions/
+
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        int n = nums.size(),count =0;
+        for( int i=0; i<n; i++ )
+        {
+            for( int j=i+1; j<n; j++)
+                if(nums[i]==nums[j])
+                    count++;
+        }
+        return count;
+    }
+};
