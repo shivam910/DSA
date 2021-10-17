2e3e15bd45ce99fdb9127413f371d02c9b07be74
Shivam Shukla <77653717+shivam910@users.noreply.github.com>
2021-10-17 15:13:59 +0530

    Create Top View of a Binary Tree.cpp

---
+++ Trees/Top View of a Binary Tree.cpp
@@ -0,0 +1,36 @@
+class Solution {
+      public: 
+      // Function that returns the top view of a binary tree from left to right
+      vector <int> topView (Node* root)
+      {
+            vector < int > ans;
+            if( !root ) return ans;
+            map <int,int> mp;
+            queue <pair <Node*, int> > q;
+            q.push({root,0});
+            while(!q.empty())
+            {
+                  auto it = q.front();
+                  q.pop();
+                  Node* node = it.first();
+                  int line = it.second();
+                  if( mp.find(line) == mp.end())
+                  mp[line] = node -> data;
+                  
+                  if( node->left != NULL )
+                  {
+                        q.push({ node->left, line-1 });
+                  }
+                  
+                  if( node->right != NULL )
+                  {
+                        q.push({ node->right, line+1 });
+                  }
+            }
+            
+            for( auto it: mp)
+            ans.push_back(it.second);
+            return ans;
+      }
+      
+};
