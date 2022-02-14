fee2f59ee06f0a4d5ff917dbe183824a26127452
Shivam Shukla <77653717+shivam910@users.noreply.github.com>
2022-02-14 11:02:23 +0530

    Create Binary Tree Level Order Traversal II.cpp

---
+++ Trees/LeetCode/Binary Tree Level Order Traversal II.cpp
@@ -0,0 +1,60 @@
+/**
+
+Given the root of a binary tree, return the bottom-up level order traversal of its nodes' values. (i.e., from left to right, level by level from leaf to root).
+
+ 
+
+Example 1:
+
+
+Input: root = [3,9,20,null,null,15,7]
+Output: [[15,7],[9,20],[3]]
+Example 2:
+
+Input: root = [1]
+Output: [[1]]
+Example 3:
+
+Input: root = []
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 2000].
+-1000 <= Node.val <= 1000
+
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        vector <vector<int>> ans;
+        if(!root) return ans;
+        queue <TreeNode*> q;
+        q.push(root);
+        while( !q.empty() )
+        {
+            vector <int> v;
+            int n = q.size();
+            for(int i=0; i<n; i++) {
+                TreeNode* temp =q.front();
+                q.pop();
+                if(temp->left) q.push(temp->left);
+                if(temp->right) q.push(temp->right);
+                v.push_back(temp->val);
+            }
+            ans.push_back(v);
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
