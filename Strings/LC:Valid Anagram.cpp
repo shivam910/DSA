081c6ddfd9625369996750d26a3a78de568d57a9
Shivam Shukla <77653717+shivam910@users.noreply.github.com>
2022-07-28 23:53:16 +0530

    Create LC:Valid Anagram.cpp

---
+++ Strings/LC:Valid Anagram.cpp
@@ -0,0 +1,21 @@
+//Link: https://leetcode.com/problems/valid-anagram/
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+         if(s.length()!=t.length())
+            return 0;
+        
+        vector<int> a(26,0), b(26,0);
+        for(int i=0;i<s.length();i++)
+        {
+            a[s[i]-'a']++;
+            b[t[i]-'a']++;
+        }
+        
+        for(int i=0;i<26;i++)
+            if(a[i]!=b[i])
+                return 0;
+        return 1;
+    }
+};
